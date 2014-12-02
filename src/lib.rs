aa4e0b1e1ae151f3ace0429087c60f738321598b
Eduard Bopp <eduard.bopp@aepsil0n.de>
2014-12-02 16:37:52 +0100

    Add associated data
    A separate structure can be constructed from the tree to associate arbitrary
    data from a closure to each node. On this structure one can then execute
    computational queries.

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,11 @@ extern crate quickcheck;
 #[cfg(test)]
 #[phase(plugin)]
 extern crate quickcheck_macros;
+#[cfg(test)]
+#[phase(plugin)]
+extern crate nalgebra;
+
+#[phase(plugin, link)] extern crate log;
 
 pub mod tree;
 pub mod util;
--- src/tree.rs
+++ src/tree.rs
@@ -1,5 +1,5 @@
 use nalgebra::{Dim, BaseFloat, FloatPnt, FloatVec, zero, POrd};
-use std::num::{Int, cast};
+use std::num::{Float, Int, cast};
 use std::cmp::partial_max;
 use std::iter::AdditiveIterator;
 use util::limits;
@@ -65,15 +65,15 @@ fn branch_dispatch<P, N>(center: &P, point: &P) -> uint
 }
 
 
-enum NodeState<O, P, N> {
+enum NodeState<O, N> {
     Empty,
     Leaf(O),
-    Branch(Vec<Node<O, P, N>>),
+    Branch(Vec<N>),
 }
 
 
 pub struct Node<O, P, N> {
-    state: Option<NodeState<O, P, N>>,
+    state: Option<NodeState<O, Node<O, P, N>>>,
     center: P,
     extent: N,
 }
@@ -94,16 +94,34 @@ impl<O, P, N, V> Node<O, P, N>
           V: FloatVec<N>,
           N: BaseFloat,
 {
-    pub fn from_vec(vec: Vec<O>) -> Node<O, P, N> {
-        let (inf, sup) = limits(vec.iter().map(|obj| obj.position()));
-        let center = (inf + sup.to_vec()) / cast(2.0f64).unwrap();
-        let extent = range(0, Dim::dim(None::<P>))
-            .fold(zero(), |max, n| partial_max(max, sup[n] - inf[n]).unwrap());
+    fn from_vec_internal(vec: Vec<O>, center: P, extent: N) -> Node<O, P, N> {
         let mut tree = Node::empty(center, extent);
         for object in vec.into_iter() {
             tree.insert(object)
         }
         tree
+    }
+
+    pub fn from_vec(vec: Vec<O>) -> Node<O, P, N> {
+        let (inf, sup) = limits(vec.iter().map(|obj| obj.position()));
+        let center = (inf + sup.to_vec()) / cast(2.0f64).unwrap();
+        let extent = range(0, Dim::dim(None::<P>))
+            .fold(zero(), |max, n| partial_max(max, sup[n] - inf[n]).unwrap());
+        Node::from_vec_internal(vec, center, extent)
+    }
+
+    pub fn from_vec_with_geometry(vec: Vec<O>, center: P, minimal_extent: N) -> Node<O, P, N> {
+        let (inf, sup) = limits(vec.iter().map(|obj| obj.position()));
+        let extent = range(0, Dim::dim(None::<P>))
+            .fold(minimal_extent, |max, n|
+                partial_max(
+                    max, partial_max(
+                        (center[n] - sup[n]).abs(),
+                        (center[n] - inf[n]).abs()
+                    ).unwrap()
+                ).unwrap()
+            );
+        Node::from_vec_internal(vec, center, extent)
     }
 }
 
@@ -134,10 +152,75 @@ impl<O, P, N, V> Node<O, P, N>
 }
 
 
+pub struct NodeWithData<O, P, N, D> {
+    state: NodeState<O, NodeWithData<O, P, N, D>>,
+    center: P,
+    extent: N,
+    data: D,
+}
+
+impl<O, P, N, D> NodeWithData<O, P, N, D>
+    where D: Clone
+{
+    pub fn new(node: Node<O, P, N>, default: D, single: |&O| -> D, combine: |&D, &D| -> D) -> NodeWithData<O, P, N, D> {
+        let (center, extent) = (node.center, node.extent);
+        let (state, data) = match node.state.unwrap() {
+            NodeState::Empty => (NodeState::Empty, default),
+            NodeState::Leaf(obj) => {
+                let value = single(&obj);
+                (NodeState::Leaf(obj), value)
+            },
+            NodeState::Branch(nodes) => {
+                let assoc_nodes: Vec<_> = nodes.into_iter()
+                    .map(|node|
+                        NodeWithData::new(node, default.clone(), |x| single(x), |x, y| combine(x, y)))
+                    .collect();
+                let value = assoc_nodes.iter().fold(default,
+                    |current, node| combine(&current, &node.data));
+                (NodeState::Branch(assoc_nodes), value)
+            }
+        };
+        NodeWithData {
+            state: state,
+            center: center,
+            extent: extent,
+            data: data,
+        }
+    }
+}
+
+impl<O, P, N, D> NodeWithData<O, P, N, D> {
+    pub fn compute<T>(&self, init: T, subdivide: |&P, &N, &D| -> bool, combine: |T, &D| -> T) -> T {
+        match self.state {
+            NodeState::Empty => {
+                debug!("empty");
+                init
+            },
+            NodeState::Leaf(_) => {
+                debug!("leaf");
+                combine(init, &self.data)
+            },
+            NodeState::Branch(ref nodes) => {
+                if subdivide(&self.center, &self.extent, &self.data) {
+                    debug!("branch subdivide");
+                    nodes.iter().fold(init, |current, node| node.compute(
+                        current,
+                        |p, n, d| subdivide(p, n, d), |t, d| combine(t, d)
+                    ))
+                } else {
+                    debug!("branch stop");
+                    combine(init, &self.data)
+                }
+            },
+        }
+    }
+}
+
+
 #[cfg(test)]
 mod test {
-    use super::{Node, Entry, NodeState, subdivide, branch_dispatch};
-    use nalgebra::{ApproxEq, Pnt2, Pnt3, FloatPnt};
+    use super::{Node, Entry, NodeState, NodeWithData, subdivide, branch_dispatch};
+    use nalgebra::{ApproxEq, Pnt2, Pnt3, FloatPnt, Vec2, Vec3, zero, Norm, Orig};
     use std::num::Float;
     use quickcheck::TestResult;
 
@@ -296,5 +379,116 @@ mod test {
                 _ => false,
             }
         )
+    }
+
+    #[quickcheck]
+    fn node_with_data_center_of_mass(data: Vec<(f64, f64, f64)>) -> TestResult {
+        // Only test non-empty lists with positive masses
+        if data.is_empty() || data.iter().any(|&(m, _, _)| m <= 0.0) {
+            return TestResult::discard();
+        }
+        // Compute center of mass in the traditional way
+        let (mps, ms) = data.iter()
+            .map(|&(m, x, y)| (Vec2::new(x, y) * m, m))
+            .fold((zero::<Vec2<f64>>(), 0.0f64), |(mps, ms), (mp, m)| (mps + mp, ms + m));
+        let com = mps / ms;
+        // Now use the tree
+        let tree = Node::from_vec(
+            data.iter()
+            .map(|&(m, x, y)| Entry { object: m, position: Pnt2::new(x, y) })
+            .collect()
+        );
+        let assoc_tree = NodeWithData::new(
+            tree,
+            (Vec2::new(0.0f64, 0.0), 0.0f64),
+            |obj| (obj.position.to_vec() * obj.object, obj.object),
+            |&(mps, ms), &(mp, m)| (mps + mp, ms + m)
+        );
+        let (tree_mps, tree_ms) = assoc_tree.data;
+        // …and compare
+        TestResult::from_bool(ApproxEq::approx_eq(&(tree_mps / tree_ms), &com))
+    }
+
+    #[quickcheck]
+    fn node_with_data_gravity(
+            starfield: Vec<(f64, (f64, f64, f64))>,
+            test_point_: (f64, f64, f64)
+        ) -> TestResult
+    {
+        // We want to have at least one star
+        if starfield.is_empty() {
+            return TestResult::discard();
+        }
+        // Only test positive masses
+        if starfield.iter().any(|&(m, _)| m <= 0.0) {
+            return TestResult::discard();
+        }
+        // The test point should not be in the same place as any star
+        if starfield.iter().any(|&(_, p)| p == test_point_) {
+            return TestResult::discard();
+        }
+        // No two stars should be in the same place
+        for i in range(0, starfield.len()) {
+            for j in range(0, i) {
+                let (_, pi) = starfield[i];
+                let (_, pj) = starfield[j];
+                if pi == pj {
+                    return TestResult::discard();
+                }
+            }
+        }
+        // (T, T, T) -> Pnt3<T>
+        let pnt = |p| {
+            let (x, y, z) = p;
+            Pnt3::new(x, y, z)
+        };
+        let test_point = pnt(test_point_);
+        // Newton's law of gravity for two point masses (with G = 1)
+        let newton = |(m, p1): (f64, Pnt3<f64>), p2| {
+            let diff: Vec3<f64> = p1 - p2;
+            let r = diff.norm();
+            diff * (m / r.powi(3))
+        };
+        // Calculate gravity exactly
+        let simple_gravity = starfield.iter()
+            .map(|&(m, p)| newton((m, pnt(p)), test_point))
+            .fold(zero(), |a: Vec3<_>, b| a + b);
+        // Calculate gravity using a tree
+        let orig: Pnt3<f64> = Orig::orig();
+        let tree = Node::from_vec_with_geometry(
+            starfield.iter()
+            .map(|&(m, (x, y, z))| Entry { object: m, position: Pnt3::new(x, y, z) })
+            .collect(),
+            orig,
+            test_point.as_vec().norm()
+        );
+        let assoc_tree = NodeWithData::new(
+            tree,
+            (orig, zero()),
+            |obj| (obj.position, obj.object),
+            |&(com1, m1), &(com2, m2)|
+                if m1 + m2 > zero() {(
+                    orig + (com1.to_vec() * m1 + com2.to_vec() * m2) / (m1 + m2),
+                    m1 + m2,
+                )}
+                else {
+                    (orig, zero())
+                }
+        );
+        let theta = 0.5; // A bit arbitrary but this appears to work
+        let tree_gravity = assoc_tree.compute(
+            Vec3::new(0.0f64, 0.0, 0.0),
+            |&node_center, &node_size, &(center_of_mass, _)| {
+                let d = FloatPnt::dist(&test_point, &center_of_mass);
+                let delta = FloatPnt::dist(&node_center, &center_of_mass);
+                d < 2.0 * node_size / theta + delta
+            },
+            |g, &(com, m)| {
+                debug!("[{}, gravity] {}, {}, {}", (&starfield, test_point_), g, m, com);
+                g + newton((m, com), test_point)
+            },
+        );
+        // Now the tree gravity should approximate the exact one, within 5 %
+        TestResult::from_bool(simple_gravity.approx_eq_eps(&tree_gravity, &(0.05 * simple_gravity.norm())))
     }
 }
